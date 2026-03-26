f1729a26e5861b7637c20417ee29cb4e2d50fa89
sergey_vetyutnev <sergey.vetyutnev@gmail.com>
2026-03-26 12:53:35 +0300

    init commit

---
+++ src/main/java/dev/vetyutnev/eventmanagerplatform/EventManagerPlatformApplication.java
@@ -0,0 +1,13 @@
+package dev.vetyutnev.eventmanagerplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagerPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/vetyutnev/eventmanagerplatform/EventManagerPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vetyutnev.eventmanagerplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
