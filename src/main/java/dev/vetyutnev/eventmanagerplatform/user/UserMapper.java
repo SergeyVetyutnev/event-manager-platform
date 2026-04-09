eb4063ab04b165ce7a86294493560a98fa9d8193
sergey_vetyutnev <sergey.vetyutnev@gmail.com>
2026-04-09 14:00:16 +0300

    добавлена доменная сущность User и его dto

---
+++ src/main/java/dev/vetyutnev/eventmanagerplatform/user/UserMapper.java
@@ -0,0 +1,8 @@
+package dev.vetyutnev.eventmanagerplatform.user;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public class UserMapper {
+}
