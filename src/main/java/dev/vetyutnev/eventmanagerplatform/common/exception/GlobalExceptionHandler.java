6c3c2734641ae4872666644c39facd246c2e9e4d
sergey_vetyutnev <sergey.vetyutnev@gmail.com>
2026-05-01 20:38:30 +0300

    добавлены обработчики исключений: EventNotFoundException, EventValidationException, EventAccessDeniedException, RegistrationException

--- src/main/java/dev/vetyutnev/eventmanagerplatform/common/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/vetyutnev/eventmanagerplatform/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package dev.vetyutnev.eventmanagerplatform.common.exception;
 
+import dev.vetyutnev.eventmanagerplatform.event.exception.EventAccessDeniedException;
+import dev.vetyutnev.eventmanagerplatform.event.exception.EventNotFoundException;
+import dev.vetyutnev.eventmanagerplatform.event.exception.EventValidationException;
+import dev.vetyutnev.eventmanagerplatform.event.registration.exception.RegistrationException;
 import dev.vetyutnev.eventmanagerplatform.location.exception.LocationNotFoundException;
 import dev.vetyutnev.eventmanagerplatform.location.exception.UserNotFoundException;
 import dev.vetyutnev.eventmanagerplatform.security.exception.InvalidCredentialException;
@@ -19,7 +23,7 @@ import java.util.stream.Collectors;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(LocationNotFoundException.class)
-    public ResponseEntity<ErrorMessageResponse> handleNotFoundException(LocationNotFoundException e){
+    public ResponseEntity<ErrorMessageResponse> handleLocationNotFoundException(LocationNotFoundException e){
         log.warn("Not found error: {}", e.getMessage());
 
         var response = new ErrorMessageResponse(
@@ -32,7 +36,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<ErrorMessageResponse> handleNotFoundException(UserNotFoundException e){
+    public ResponseEntity<ErrorMessageResponse> handleUserNotFoundException(UserNotFoundException e){
         log.warn("Not found error: {}", e.getMessage());
 
         var response = new ErrorMessageResponse(
@@ -62,8 +66,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(InvalidCredentialException.class)
-    public ResponseEntity<ErrorMessageResponse>
-    handleInvalidCredentialException(InvalidCredentialException e){
+    public ResponseEntity<ErrorMessageResponse> handleInvalidCredentialException(InvalidCredentialException e){
         log.warn("Auth error: {}", e.getMessage());
         var response = new ErrorMessageResponse(
                 "необходима аутентификация",
@@ -71,6 +74,39 @@ public class GlobalExceptionHandler {
                 LocalDateTime.now()
         );
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+    }
+
+    @ExceptionHandler(EventNotFoundException.class)
+    public ResponseEntity<ErrorMessageResponse> handleEventNotFoundException(EventNotFoundException e) {
+        log.warn("Event not found: {}", e.getMessage());
+        var response = new ErrorMessageResponse(
+                "мероприятие не найдено",
+                e.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(EventAccessDeniedException.class)
+    public ResponseEntity<ErrorMessageResponse> handleEventAccessDeniedException(EventAccessDeniedException ex) {
+        log.warn("Event access denied: {}", ex.getMessage());
+        var response = new ErrorMessageResponse(
+                "Недостаточно прав для выполнения операции",
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
+    }
+
+    @ExceptionHandler({EventValidationException.class, RegistrationException.class})
+    public ResponseEntity<ErrorMessageResponse> handleBusinessValidationExceptions(RuntimeException ex) {
+        log.warn("Business validation error: {}", ex.getMessage());
+        var response = new ErrorMessageResponse(
+                "Некорректный запрос",
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     @ExceptionHandler(Exception.class)
